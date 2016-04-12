3249be64bbd34783af7a20f0a548a236d08e8eaf
Kevin-Bannier <bannier.kevin@orange.fr>
2016-04-12 10:43:16 +0200

    PatchMatch avec le textureDescriptor static

--- patchMatching/src/main.cpp
+++ patchMatching/src/main.cpp
@@ -5,6 +5,7 @@
 #include "PatchMatchY.h"
 #include "PatchMatchLab.h"
 #include "PatchMatchDescriptor.h"
+#include "PatchMatchDescriptorStatic.h"
 
 
 
@@ -13,20 +14,27 @@ int main(int argc,char **argv){
 	
 	source = cv::imread( "../data/sonne1.png");
 	target = cv::imread( "../data/sonne2.png");
-
+/*
 	// Distance Y
 	cv::Mat patchMatchY = PatchMatchY::apply(source,target, 5, 5);
 	imwrite( "../result/patchMatch_Y.png", patchMatchY );
-
+*/
+/*
 	// Distance Lab
 	cv::Mat patchMatchLab = PatchMatchLab::apply(source,target, 5, 5);
 	imwrite( "../result/patchMatch_Lab.png", patchMatchLab );
 
-
-
+*/
+/*
 	// Distance entre descripteurs
 	cv::Mat patchMatchDescripteurs = PatchMatchDescriptor::apply(source,target, 5, 5);
 	imwrite( "../result/patchMatch_descripteurs.png", patchMatchDescripteurs);
+*/
+
+	// Distance entre descripteurs static (non objet)
+	cv::Mat patchMatchDescripteursStatic = PatchMatchDescriptorStatic::apply(source,target, 5, 5);
+	imwrite( "../result/patchMatch_descripteurs_static.png", patchMatchDescripteursStatic);
+
 
 	return 0; 
 }
